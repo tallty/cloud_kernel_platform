e0f611227dafe9e88ec3a38833e48fd7ff8b3eb9
shtzr840329 <shtzr840329@gmail.com>
2015-08-14 03:43:49 +0800

    add station info seed, and write to redis

--- app/models/station_info.rb
+++ app/models/station_info.rb
@@ -1,2 +1,24 @@
 class StationInfo < ActiveRecord::Base
+
+  def as_json(options=nil)
+    {
+      site_number: site_number,
+      name: alias_name,
+      district: district,
+      site_type: site_type,
+      lon: lon,
+      lat: lat,
+      province: province,
+      site_type: site_type,
+      subjection: subjection
+    }
+  end
+
+  def write_to_redis
+    $redis.del "station_infos"
+    station_infos = StationInfo.all
+    station_infos.each do |station|
+      $redis.hset "station_infos", station.site_number, station.to_json
+    end
+  end
 end
