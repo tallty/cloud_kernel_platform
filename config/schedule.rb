02b507eb2f7000d1fefc3874ceadb10adae1b17e
shtzr840329 <shtzr840329@gmail.com>
2015-09-28 21:19:29 +0800

    add stable stations task

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,6 +9,10 @@ set :job_template, "/usr/bin/timeout 1800 /bin/bash -l -c ':job'"
 # Learn more: http://github.com/javan/whenever
 
 every 1.minutes do
+  runner "StableStation.process"
+end
+
+every 1.minutes do
   runner "CommunityWarning.new.process"
 end
 
