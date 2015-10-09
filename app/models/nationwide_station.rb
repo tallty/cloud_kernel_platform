540761ccf15fbc1fb379e639d1b8b62952e819a0
shtzr840329 <shtzr840329@gmail.com>
2015-10-10 00:31:47 +0800

    perfect data process

--- app/models/nationwide_station.rb
+++ app/models/nationwide_station.rb
@@ -22,7 +22,28 @@ class NationwideStation < ActiveRecord::Base
 
     def process
       content = get_data
-      p content
+      ds = content["DS"]
+      report_time_string = ds.first["Datetime"]
+      report_time = Time.parse(report_time_string) + 8.hour
+      group = NationwideStation.find_or_create_by report_date: report_time
+
+      ds.each do |item|
+        sitenumber = item["Station_Id_C"]
+        city = StationInfo.find_city_from_redis sitenumber
+        return unless city.present?
+        item = group.nationwide_station_items.find_or_create_by datetime: report_time, sitenumber: sitenumber.to_s
+        item.name = city["name"]
+        item.tempe = line['TEM'].to_f
+        item.rain = line['PRE'].to_f
+        item.wind_direction = line['WIN_D_INST'].to_f
+        item.wind_speed = line['WIN_S_INST'].to_f
+        item.visibility = line['VIS_HOR_1MI'].to_f
+        item.pressure = line['PRS'].to_f
+        item.humi = line['RHU'].to_f
+        $redis.hset "#{@redis_key}", item.name.sub(/市|新区|区|县|乡|镇/, ''), item.to_json
+        
+        item.save
+      end
     end
 
     def get_data
