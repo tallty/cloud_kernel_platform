a3cb48db68c2216a441ea926c829cca6f060998a
shtzr840329 <shtzr840329@gmail.com>
2015-10-09 23:11:33 +0800

    fix

--- app/models/nationwide_station.rb
+++ app/models/nationwide_station.rb
@@ -1,5 +1,6 @@
 class NationwideStation < ActiveRecord::Base
   establish_connection :old_database
   self.table_name = "cs_groups"
+  set_primary_key 'cs_group_id'
   has_many :nationwide_station_items
 end
--- app/models/nationwide_station_item.rb
+++ app/models/nationwide_station_item.rb
@@ -1,6 +1,5 @@
 class NationwideStationItem < ActiveRecord::Base
   establish_connection :old_database
   self.table_name = "cs_items"
-  set_primary_key 'cs_group_id'
   belongs_to :nationwide_station
 end
