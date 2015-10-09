9dfd0e0d63c9574274357d9e90c88f7b5bca7321
shtzr840329 <shtzr840329@gmail.com>
2015-10-09 23:09:03 +0800

    fix primary key

--- app/models/nationwide_station_item.rb
+++ app/models/nationwide_station_item.rb
@@ -1,5 +1,6 @@
 class NationwideStationItem < ActiveRecord::Base
   establish_connection :old_database
   self.table_name = "cs_items"
+  set_primary_key 'cs_group_id'
   belongs_to :nationwide_station
 end
