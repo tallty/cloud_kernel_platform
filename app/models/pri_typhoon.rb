3f609d3c8eb88494c0e59f38df4e2ac2cc62d794
liyijie <liyijie825@gmail.com>
2016-09-12 01:41:03 +0800

    pri_typhoon source fix, the base version, debug by the server

--- app/models/pri_typhoon.rb
+++ app/models/pri_typhoon.rb
@@ -74,70 +74,169 @@ class PriTyphoon < ActiveRecord::Base
     end
   end
 
+  # def self.get_now_typhoon
+  #   typhoons = PriTyphoon::PriTyphoonProcess.new.fetch({action: 'nowtyphoon'})
+  #   now_typhoons = []
+  #   typhoons.each do |item|
+  #     serial_number = item['TFBH'][-4, 4]
+  #     typhoon = PriTyphoon.find_by(serial_number: serial_number)
+  #     if typhoon.blank?
+  #       typhoon = get_typhoon_list serial_number
+  #     end
+  #     typhoon.update_attributes(status: 1)
+  #     typhoon.refresh_typhoon_detail item
+  #     now_typhoons << serial_number
+  #   end
+  #   $redis.set "now_typhoons_cache", now_typhoons.join(',')
+  # end
+
+  # 20160912
   def self.get_now_typhoon
-    typhoons = PriTyphoon::PriTyphoonProcess.new.fetch({action: 'nowtyphoon'})
-    now_typhoons = []
+    _year = DateTime.now.year
+    _host = "http://typhoon.shmmc.cn"
+    _url = "/TyphoonLine/PatrolHandler.ashx?provider=Readearth.PublicSrviceGIS.BLL.TyphoonBLL&assembly=Readearth.PublicSrviceGIS.BLL&method=GetTyhoonByYear&year=#{_year}"
+
+    connect = Faraday.new(url: _host) do |faraday|
+      faraday.request :url_encoded
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = connect.get do |request|
+      request.url _url
+      request.headers['Content-Type'] = 'application/json'
+      request.headers['Accept'] = 'application/json'
+      request_params = { year: _year }
+    end
+    typhoons = MultiJson.load(response.body)
     typhoons.each do |item|
-      serial_number = item['TFBH'][-4, 4]
+      serial_number = item['tfbh'][-4, 4]
       typhoon = PriTyphoon.find_by(serial_number: serial_number)
       if typhoon.blank?
-        typhoon = get_typhoon_list serial_number
+        typhoon = PriTyphoon.find_or_create_by serial_number: serial_number
+        typhoon.cname = item['name'] if typhoon.cname.blank?
+        typhoon.ename = item['ename'] if typhoon.ename.blank?
+        typhoon.year = _year
+        typhoon.save
       end
       typhoon.update_attributes(status: 1)
       typhoon.refresh_typhoon_detail item
-      now_typhoons << serial_number
     end
-    $redis.set "now_typhoons_cache", now_typhoons.join(',')
   end
 
   def refresh_typhoon_detail params
-    result = PriTyphoon::PriTyphoonProcess.new.fetch params
 
-    # typhoon_info = result['tfbh']
-    # typhoon = PriTyphoon.find_or_create_by serial_number: typhoon_info['TFBH'][-4, 4]
-    # reutrn if typhoon.try(:status) == 1
-    # typhoon.cname = typhoon_info['TFM'] if typhoon.cname.blank?
-    # typhoon.ename = typhoon_info['TFME'] if typhoon.ename.blank?
-    # typhoon.year = typhoon_info['TFBH'][0, 4]
+    _host = "http://typhoon.shmmc.cn"
+    _url = "/TyphoonLine/PatrolHandler.ashx?provider=Readearth.PublicSrviceGIS.BLL.TyphoonBLL&assembly=Readearth.PublicSrviceGIS.BLL&method=GetTyphoonDetail&sno=#{params['tfbh']}"
 
-    real_location = result['tflslj']
+    connect = Faraday.new(url: _host) do |faraday|
+      faraday.request :url_encoded
+      faraday.response :logger
+      faraday.adapter Faraday.default_adapter
+    end
+
+    response = connect.get do |request|
+      request.url _url
+      request.headers['Content-Type'] = 'application/json'
+      request.headers['Accept'] = 'application/json'
+      request_params = { year: _year }
+    end
+    result = MultiJson.load(response.body)
+
+    real_location = result['points']
 
     last_forecast_time = {}
     last_report_time = nil
     real_location.each do |item|
-      _item = pri_typhoon_items.find_or_create_by info: 0, cur_time: Time.zone.parse(item['RQSJ'])
-      _item.lon = item['JD']
-      _item.lat = item['WD']
-      _item.min_pressure = item['ZXQY']
-      _item.max_wind = item['ZXFS']
-      _item.move_speed = item['YDSD']
-      _item.move_direction = item['YDFX']
-      _item.seven_radius = item['RADIUS7']
-      _item.ten_radius = item['RADIUS10']
+      _item = pri_typhoon_items.find_or_create_by info: 0, cur_time: Time.zone.parse(item['time'])
+      _item.lon = item['longitude']
+      _item.lat = item['latitude']
+      _item.min_pressure = item['pressure']
+      _item.max_wind = item['speed']
+      _item.move_speed = item['move_speed']
+      _item.move_direction = item['move_dir']
+      _item.seven_radius = item['radius7']
+      _item.ten_radius = item['radius10']
       last_report_time = _item.cur_time
       _item.save
     end
     self.update_attributes(last_report_time: last_report_time)
 
-    forecast_location = result['tfyblj']
-    forecast_location.each do |item|
-      _item = pri_typhoon_items.find_or_create_by info: 1, cur_time: Time.zone.parse(item['RQSJ']), report_time: Time.zone.parse(item['YBSJ']), unit: item['TM']
-      _item.lon = item['JD']
-      _item.lat = item['WD']
-      _item.min_pressure = item['ZXQY']
-      _item.max_wind = item['ZXFS']
-      _item.move_speed = item['YDSD']
-      _item.move_direction = item['YDFX']
-      _item.seven_radius = item['RADIUS7']
-      _item.ten_radius = item['RADIUS10']
-      last_forecast_time[_item.unit] = _item.report_time
-      _item.save
+    last_real_location = real_location.last
+    forecast_sets = last_real_location['forecast']
+    forecast_sets.each do |forecast_set|
+      unit = forecast_set['sets']
+      forecast_location = forecast_set['points']
+      forecast_location.each do |item|
+        _item = pri_typhoon_items.find_or_create_by info: 1, cur_time: Time.zone.parse(last_real_location['time']), report_time: Time.zone.parse(item['time']), unit: unit
+        _item.lon = item['longitude']
+        _item.lat = item['latitude']
+        _item.min_pressure = item['pressure']
+        _item.max_wind = item['speed']
+        _item.move_speed = item['move_speed']
+        _item.move_direction = item['move_dir']
+        _item.seven_radius = item['radius7']
+        _item.ten_radius = item['radius10']
+        last_forecast_time[_item.unit] = _item.report_time
+        _item.save
+      end
     end
+    
 
     build_content last_forecast_time
 
     nil
   end
+
+
+  # def refresh_typhoon_detail params
+  #   result = PriTyphoon::PriTyphoonProcess.new.fetch params
+
+  #   # typhoon_info = result['tfbh']
+  #   # typhoon = PriTyphoon.find_or_create_by serial_number: typhoon_info['TFBH'][-4, 4]
+  #   # reutrn if typhoon.try(:status) == 1
+  #   # typhoon.cname = typhoon_info['TFM'] if typhoon.cname.blank?
+  #   # typhoon.ename = typhoon_info['TFME'] if typhoon.ename.blank?
+  #   # typhoon.year = typhoon_info['TFBH'][0, 4]
+
+  #   real_location = result['tflslj']
+
+  #   last_forecast_time = {}
+  #   last_report_time = nil
+  #   real_location.each do |item|
+  #     _item = pri_typhoon_items.find_or_create_by info: 0, cur_time: Time.zone.parse(item['RQSJ'])
+  #     _item.lon = item['JD']
+  #     _item.lat = item['WD']
+  #     _item.min_pressure = item['ZXQY']
+  #     _item.max_wind = item['ZXFS']
+  #     _item.move_speed = item['YDSD']
+  #     _item.move_direction = item['YDFX']
+  #     _item.seven_radius = item['RADIUS7']
+  #     _item.ten_radius = item['RADIUS10']
+  #     last_report_time = _item.cur_time
+  #     _item.save
+  #   end
+  #   self.update_attributes(last_report_time: last_report_time)
+
+  #   forecast_location = result['tfyblj']
+  #   forecast_location.each do |item|
+  #     _item = pri_typhoon_items.find_or_create_by info: 1, cur_time: Time.zone.parse(item['RQSJ']), report_time: Time.zone.parse(item['YBSJ']), unit: item['TM']
+  #     _item.lon = item['JD']
+  #     _item.lat = item['WD']
+  #     _item.min_pressure = item['ZXQY']
+  #     _item.max_wind = item['ZXFS']
+  #     _item.move_speed = item['YDSD']
+  #     _item.move_direction = item['YDFX']
+  #     _item.seven_radius = item['RADIUS7']
+  #     _item.ten_radius = item['RADIUS10']
+  #     last_forecast_time[_item.unit] = _item.report_time
+  #     _item.save
+  #   end
+
+  #   build_content last_forecast_time
+
+  #   nil
+  # end
 
   def build_content last_forecast_time
     real_path = pri_typhoon_items.where(info: 0)
