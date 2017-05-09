db0cc9e79aedc05e1579bc86f356176ac768274a
liyijie <liyijie825@gmail.com>
2017-05-09 21:51:35 +0800

    Update schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,9 +24,9 @@ every 1.minutes do
   runner "CommunityWarning.new.process"
 end
 
-every 1.minutes do
-  runner "GridLive.process"
-end
+# every 1.minutes do
+#   runner "GridLive.process"
+# end
 
 # every 3.minutes do
 #   # runner "QPF.new.process"
@@ -55,9 +55,9 @@ every 5.minutes do
   runner "WorldForecast.new.process"
 end
 
-every 5.minutes do
-  runner "Typhoon.process"
-end
+# every 5.minutes do
+  # runner "Typhoon.process"
+# end
 
 every 10.minutes do
   runner "ExchangeFile.process"
