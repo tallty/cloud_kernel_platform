94c3f121b8d336d05c0b635e78e0631feb73b4b3
ivan Lan <mumumumushu@gmail.com>
2017-10-27 11:19:34 +0800

    Fix PriTyphoon.new.refresh_detail

--- app/models/pri_typhoon.rb
+++ app/models/pri_typhoon.rb
@@ -77,7 +77,7 @@ class PriTyphoon < ActiveRecord::Base
           cname: item['name'],
           ename: item['enname'],
         )
-        typhoon.refresh_detail if is_current == 1
+        typhoon.refresh_detail #if is_current == 1
       end
     end
 
